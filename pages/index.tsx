dae0613bcbd4c26fbbcd71a62d2f6b22611c5e8d
Bart Janeczko <bartjaneczko@gmail.com>
2022-11-06 17:32:40 +0100

    UI layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,18 @@
+import GradientLayout from '../components/gradientLayout'
+
 const Home = () => {
-  return <div>HOME</div>
+  return (
+    <GradientLayout
+      color="purple"
+      subtitle="profile"
+      title="Bart Janeczko"
+      description="15 public playlists"
+      image="https://store.playstation.com/store/api/chihiro/00_09_000/container/GR/en/19/EP2402-CUSA05624_00-AV00000000000156/image?w=320&h=320&bg_color=000000&opacity=100&_version=00_09_000"
+      roundImage
+    >
+      <div>Home page</div>
+    </GradientLayout>
+  )
 }
 
 export default Home
